53f2a7e2dbff733365b2090aceafbb209fcdc4ca
brunna monteiro <brunnabrauner@gmail.com>
2023-08-15 16:38:40 +0200

    Created form for new add task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,26 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.new(tasks_params)
+    task.save
+
+    redirect_to tasks_path(task)
+  end
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(:title, :details)
+  end
 end
